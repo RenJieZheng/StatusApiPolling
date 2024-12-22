ae8d6263efa7d6ea84558ef2108a7dfb6266c9c0
RenJieZheng <42659888+RenJieZheng@users.noreply.github.com>
2024-12-22 18:11:30 -0500

    Integration tests for PollUntilCompleted

--- Status.IntegrationTests/ClientApiIntegrationTests.cs
+++ Status.IntegrationTests/ClientApiIntegrationTests.cs
@@ -19,11 +19,11 @@ public class ClientApiIntegrationTests
     {
         // Arrange
         var client = _factory.CreateClient();
-        StatusClient statusClient = new StatusClient(client);
+        StatusClient statusClient = new StatusClient(client, 100, 2, 30000);
         using StringContent jsonContent = new("", Encoding.UTF8, "application/json");
 
         // Act
-        var response = await client.PostAsync("/job", jsonContent);
+        await client.PostAsync("/job", jsonContent);
         var response1 = await statusClient.GetStatusAsync();
         Thread.Sleep(5100);
         var response2 = await statusClient.GetStatusAsync();
@@ -32,4 +32,36 @@ public class ClientApiIntegrationTests
         Assert.Equal("pending", response1.result);
         Assert.Equal("completed", response2.result);
     }    
+
+    [Fact]
+    public async Task StatusClient_PollUntilCompletedReturnsCompleted()
+    {
+         // Arrange
+        var client = _factory.CreateClient();
+        StatusClient statusClient = new StatusClient(client, 100, 2, 30000);
+        using StringContent jsonContent = new("", Encoding.UTF8, "application/json");
+
+        // Act
+        await client.PostAsync("/job", jsonContent);
+        var response = await statusClient.PollUntilCompleted();
+
+        // Assert
+        Assert.Equal("completed", response.result);
+    }
+
+    [Fact]
+    public async Task StatusClient_PollUntilCompletedTimesOut()
+    {
+         // Arrange
+        var client = _factory.CreateClient();
+        StatusClient statusClient = new StatusClient(client, 100, 2, 4000);
+        using StringContent jsonContent = new("", Encoding.UTF8, "application/json");
+
+        // Act and Assert
+        await client.PostAsync("/job", jsonContent);
+        var exception = await Assert.ThrowsAsync<TimeoutException>(
+            statusClient.PollUntilCompleted
+        );
+        Assert.Contains("Polling operation timed out", exception.Message);
+    }
 }
